55a918e281e61cae7986163472f41a0129da868a
Chris Dickinson <christopher.s.dickinson@gmail.com>
2020-10-09 19:28:13 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# mdn-cli
+
+A tiny CLI for searching the Mozilla Developer Network docs and displaying them in terminal.
+
+```
+$ mdn accept header
+
+# Accept
+
+The **`Accept`** request HTTP header advertises which content types, expressed as [MIME types][1], the client is able to understand. Using [content negotiation][2],
+the server then selects one of the proposals, uses it and informs the client of its choice with the [`Content-Type`][3] response header. Browsers set adequate values
+for this header depending on the context where the request is done: when fetching a CSS stylesheet a different value is set for the request than when fetching an
+image, video or a script.
+
+[...]
+```
+
+# Installation
+
+TODO
+
+# License
+
+MIT
---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::io::Write;
+
+#[async_std::main]
+async fn main() -> Result<(), surf::Error> {
+    //→ curl -I 'https://api.duckduckgo.com/?format=json&q=%21%20site%3Adeveloper.mozilla.org%20accept-header'
+    let args: Vec<String> = std::iter::once("! site:developer.mozilla.org".to_string())
+        .chain(std::env::args().skip(1)).collect();
+    let args = args.join(" ");
+    let query = urlencoding::encode(args.as_str());
+
+    let url = format!("https://api.duckduckgo.com/?q={}&format=json", query);
+    let response = surf::get(url.as_str()).await?;
+    let location = response.header("location")
+        .map(|xs| xs.as_str().to_owned())
+        .unwrap_or_else(Default::default);
+
+    let body = surf::get(location.as_str()).recv_string().await?;
+
+    let document = scraper::Html::parse_document(body.as_str()); 
+    let title_h1_sel = scraper::Selector::parse("header h1.title").unwrap();
+    let article_sel = scraper::Selector::parse("article#wikiArticle").unwrap();
+
+    let title = document.select(&title_h1_sel).next().map(|xs| {
+        xs.inner_html()
+    }).unwrap_or_else(|| "".to_string());
+
+    let article = document.select(&article_sel).next().map(|xs| {
+        xs.inner_html()
+    }).unwrap_or_else(|| "".to_string());
+
+    let html = format!("<h1>{}</h1><article>{}</article>", title, article);
+
+    let term_width = if let Some((w, _)) = term_size::dimensions() {
+        w
+    } else {
+        120
+    };
+
+    std::io::stdout().write_all(html2text::from_read(html.as_bytes(), term_width).as_bytes())?;
+
+    Ok(())
+}
