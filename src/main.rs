ce2bfd0f5b58d04d8908a0636999ce4cb6b47f15
Chris Dickinson <christopher.s.dickinson@gmail.com>
2020-10-10 15:19:05 -0700

    feat: write doc to pager

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::io::Write;
-use std::sync::{ Arc, atomic::{ AtomicBool, Ordering } };
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
 use std::time::Duration;
 
 #[async_std::main]
 async fn main() -> Result<(), surf::Error> {
     //→ curl -I 'https://api.duckduckgo.com/?format=json&q=%21%20site%3Adeveloper.mozilla.org%20accept-header'
     let args: Vec<String> = std::iter::once("! site:developer.mozilla.org".to_string())
-        .chain(std::env::args().skip(1)).collect();
+        .chain(std::env::args().skip(1))
+        .collect();
     let args = args.join(" ");
     let query = urlencoding::encode(args.as_str());
 
@@ -20,35 +24,40 @@ async fn main() -> Result<(), surf::Error> {
 
             async_std::task::sleep(Duration::from_millis(32)).await;
             if !loading_clone.load(Ordering::Relaxed) {
-                break
+                break;
             }
         }
     });
 
     let url = format!("https://api.duckduckgo.com/?q={}&format=json", query);
     let response = surf::get(url.as_str()).await?;
-    let location = response.header("location")
+    let location = response
+        .header("location")
         .map(|xs| xs.as_str().to_owned())
         .unwrap_or_else(Default::default);
 
     if location.is_empty() {
         std::io::stdout().write_all(b"\rNo results.")?;
-        return Ok(())
+        return Ok(());
     }
 
     let body = surf::get(location.as_str()).recv_string().await?;
 
-    let document = scraper::Html::parse_document(body.as_str()); 
+    let document = scraper::Html::parse_document(body.as_str());
     let title_h1_sel = scraper::Selector::parse("header h1.title").unwrap();
     let article_sel = scraper::Selector::parse("article#wikiArticle").unwrap();
 
-    let title = document.select(&title_h1_sel).next().map(|xs| {
-        xs.inner_html()
-    }).unwrap_or_else(|| "".to_string());
+    let title = document
+        .select(&title_h1_sel)
+        .next()
+        .map(|xs| xs.inner_html())
+        .unwrap_or_else(|| "".to_string());
 
-    let article = document.select(&article_sel).next().map(|xs| {
-        xs.inner_html()
-    }).unwrap_or_else(|| "".to_string());
+    let article = document
+        .select(&article_sel)
+        .next()
+        .map(|xs| xs.inner_html())
+        .unwrap_or_else(|| "".to_string());
 
     let html = format!("<h1>{}</h1><article>{}</article>", title, article);
 
@@ -61,7 +70,8 @@ async fn main() -> Result<(), surf::Error> {
     loading.swap(false, Ordering::Relaxed);
     async_std::task::sleep(Duration::from_millis(200)).await;
 
-    std::io::stdout().write_all(b"\r")?;
+    pager::Pager::with_pager("less -r").setup();
+    std::io::stdout().write_all(b"\x1b[4m(Results from DuckDuckGo)\x1b[0m\n\n")?;
     std::io::stdout().write_all(html2text::from_read(html.as_bytes(), term_width).as_bytes())?;
 
     Ok(())
